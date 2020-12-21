55d6098196402a2e30b5712f55b9c522fdfd39ac
Fr3aky <mikevandenhoek@quicknet.nl>
2020-12-21 22:14:45 +0100

    Styled header
    Placed everything in the right position and the search button can now toggle a search form on screen.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+
+firstButton = document.querySelector("header nav ul:nth-of-type(2) li:nth-of-type(5) button:nth-of-type(1)")
+
+firstButton.addEventListener("click", moveSearchForm);
+
+function moveSearchForm() {
+    let moveSearchForm = document.querySelector("header nav ul:nth-of-type(2) li:nth-of-type(6)");
+    moveSearchForm.classList.toggle("moveForm");
+}
