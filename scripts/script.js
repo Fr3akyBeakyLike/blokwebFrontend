95eefe43660156d5a5dcd3e265135b3bba130e0d
Fr3aky <mikevandenhoek@quicknet.nl>
2020-12-31 14:22:39 +0100

    Added the black overlay on the list
    When clicking the search button now there will be an black overlay. When you click on it the overlay and search form move away.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 // JavaScript Document
 
 firstButton = document.querySelector("header nav ul:nth-of-type(2) li:nth-of-type(5) button:nth-of-type(1)")
+incomingList = document.querySelector("header nav ul:nth-of-type(2) li:nth-of-type(6)")
 
+incomingList.addEventListener("click", moveSearchForm)
 firstButton.addEventListener("click", moveSearchForm);
 
 function moveSearchForm() {
