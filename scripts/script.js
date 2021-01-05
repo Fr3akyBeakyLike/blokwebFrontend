51d515bed263e5f8ef21cf53fc11363cc9022aa8
Fr3aky <mikevandenhoek@quicknet.nl>
2021-01-05 21:41:54 +0100

    Articles and aside added to agenda page

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 // JavaScript Document
 
+// Melkweg home page
 firstButton = document.querySelector("header nav ul:nth-of-type(2) li:nth-of-type(5) button:nth-of-type(1)")
 incomingList = document.querySelector("header nav ul:nth-of-type(2) li:nth-of-type(6)")
 
@@ -9,4 +10,14 @@ firstButton.addEventListener("click", moveSearchForm);
 function moveSearchForm() {
     let moveSearchForm = document.querySelector("header nav ul:nth-of-type(2) li:nth-of-type(6)");
     moveSearchForm.classList.toggle("moveForm");
+}
+
+// Agenda page
+filterButton = document.querySelector("header nav#agenda il li:nth-of-type(1) button")
+
+filterButton.addEventListener("click", showFilters);
+
+function showFilters() {
+    let showFilters = document.querySelector("")
+    showFilters.classList.toggle("")
 }
